3a0f948066dcddec59dbef83dafbfe9353fc9223
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-09-25 17:25:47 +0700

    feat: create home page ui

--- src/app.js
+++ src/app.js
@@ -15,6 +15,9 @@ const PORT = process.env.PORT || 8000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
+
 app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
