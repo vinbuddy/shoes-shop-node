a9e16fd9c0cc65a9d8edb5cee09b47d6a495ebf7
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-09-30 09:20:34 +0700

    refactor: remove home bg, create cart page ui

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,3 +1,3 @@
 export function renderCartPage(req, res) {
-    return res.render("cart/index", { layout: "./layouts/main", page: "home", title: "Home page" });
+    return res.render("cart/index", { layout: "./layouts/main", page: "cart", title: "Cart page" });
 }
