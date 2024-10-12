07fdeb0e4ba6ffc075d20a9cec94bbc2d12343bb
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-10-12 10:38:43 +0700

    feat(register): implement register and verify otp

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -13,20 +13,24 @@ export async function renderRegisterPage(req, res) {
     return res.render("auth/register", { layout: "./layouts/auth" });
 }
 
+export async function renderVerifyOTPPage(req, res) {
+    return res.render("auth/otp", { layout: "./layouts/auth" });
+}
+
+//  HANDLERS
 export async function registerHandler(req, res) {
     try {
         const { email, password, username } = req.body;
 
-        const isExists = await CustomerModel.findOne({
+        const customer = await CustomerModel.findOne({
             email: email,
-            isVerified: true,
         });
 
         // Check if email already exists, redirect to register page with error message
-        if (isExists) {
+        if (customer && customer.isVerified) {
             return res.render("auth/register", {
                 layout: "./layouts/auth",
-                error: "Email đã tồn tại.",
+                error: "Người dùng đã tồn tại.",
             });
         }
 
@@ -35,9 +39,7 @@ export async function registerHandler(req, res) {
         const existingOTP = await redisClient.get(email);
 
         if (existingOTP) {
-            // If OTP exists, delete it
             await redisClient.del(email);
-            console.log(`Deleted existing OTP for ${email}`);
         }
 
         const otp = otpGenerator.generate(6, {
@@ -49,7 +51,6 @@ export async function registerHandler(req, res) {
 
         // Set OTP to Redis with 5 minutes expiration
         await redisClient.setEx(email, 300, otp);
-        console.log(`OTP for ${email} is set in Redis: ${otp}`);
 
         // Create new customer with isVerified: false
         const newCustomer = new CustomerModel({
@@ -67,12 +68,70 @@ export async function registerHandler(req, res) {
             `Your OTP is ${otp}. This OTP will expire in 5 minutes.`
         );
 
-        return res.render("auth/verify-otp", {
+        return res.render("auth/otp", {
             layout: "./layouts/auth",
             email,
             message: "OTP đã gửi đến email của bạn",
         });
     } catch (error) {
         return res.status(500).send({ message: error.message });
+    }
+}
+
+export async function verifyOTPHandler(req, res) {
+    try {
+        const { email, otp } = req.body;
+
+        if (!email) {
+            return res.render("auth/otp", {
+                layout: "./layouts/auth",
+                error: "Email không có giá trị",
+            });
+        }
+
+        if (!otp) {
+            return res.render("auth/otp", {
+                layout: "./layouts/auth",
+                email,
+                error: "OTP không có giá trị",
+            });
+        }
+
+        const redisClient = getRedis();
+
+        const existingOTP = await redisClient.get(email);
+
+        if (!existingOTP) {
+            return res.render("auth/otp", {
+                layout: "./layouts/auth",
+                email,
+                error: "OTP không hợp lệ hoặc đã hết hạn.",
+            });
+        }
+
+        if (existingOTP !== otp) {
+            return res.render("auth/otp", {
+                layout: "./layouts/auth",
+                email,
+                error: "OTP không hợp lệ.",
+            });
+        }
+
+        // If OTP is correct, delete it from Redis
+        await redisClient.del(email);
+
+        // Update isVerified to true
+        await CustomerModel.updateOne({ email: email }, { isVerified: true });
+
+        // Create session
+        const customer = await CustomerModel.findOne({ email: email });
+        req.session.customer = customer;
+
+        return res.redirect("/");
+    } catch (error) {
+        return res.render("auth/otp", {
+            layout: "./layouts/auth",
+            error: error.message,
+        });
     }
 }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { renderLoginPage, renderRegisterPage, registerHandler } from "../controllers/auth.controller.js";
+import {
+    renderLoginPage,
+    renderRegisterPage,
+    registerHandler,
+    renderVerifyOTPPage,
+    verifyOTPHandler,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.get("/login", renderLoginPage);
 router.get("/register", renderRegisterPage);
+router.get("/verify-otp", renderVerifyOTPPage);
 
 router.post("/register", registerHandler);
+router.post("/verify-otp", verifyOTPHandler);
 
 export default router;
