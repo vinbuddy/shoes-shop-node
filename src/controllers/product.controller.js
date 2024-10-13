11996ef57260449822b2849de9cab10ff9615322
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-10-13 21:56:26 +0700

    feat(product): render category, brand, size and products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,38 +1,70 @@
-// Fake product data
-const products = [
-    {
-        id: "1",
-        name: "Product 1",
-        price: 100,
-        description: "This is a product description",
-        quantity: 10,
-    },
-    {
-        id: "2",
-        name: "Product 2",
-        price: 200,
-        description: "This is a product description",
-        quantity: 20,
-    },
-];
+import BrandModel from "../models/brand.model.js";
+import CategoryModel from "../models/category.model.js";
+import ProductModel from "../models/product.model.js";
+import SizeModel from "../models/size.model.js";
+import { formatVNCurrency } from "../utils/format.js";
 
-export function renderProductPage(req, res) {
-    // Get product data from database here
+export async function renderProductPage(req, res) {
+    const brands = await BrandModel.find();
+    const categories = await CategoryModel.find();
+    const sizes = await SizeModel.find();
+
+    const { page = 1, name, brand, category, size } = req.query;
+    const pageSize = 10;
+
+    const filters = {
+        isDeleted: false,
+    };
+
+    if (name && name.trim()) {
+        filters.name = { $regex: new RegExp(name, "i") };
+    }
+
+    if (brand) {
+        const brandArray = brand.split("-");
+        filters.brand = { $in: brandArray };
+    }
+
+    if (category) {
+        const categoryArray = category.split("-");
+        filters.categories = categoryArray;
+    }
+
+    if (size) {
+        const sizeArray = size.split("-");
+        filters["sizes.size"] = { $in: sizeArray };
+    }
+
+    const products = await ProductModel.find(filters)
+        .populate("brand")
+        .populate("categories")
+        .populate("sizes.size")
+        .limit(pageSize)
+        .skip((page - 1) * pageSize);
+
+    const totalProducts = await ProductModel.countDocuments(filters);
 
     return res.render("product/index", {
         layout: "./layouts/main",
         page: "product",
-        title: "Product page",
+        title: "Danh sách sản phẩm",
+        brands: brands,
+        categories: categories,
+        sizes: sizes,
         products: products,
+        currentPage: page,
+        totalPages: Math.ceil(totalProducts / pageSize),
+        filters: req.query,
+        formatCurrency: formatVNCurrency,
     });
 }
 
 export function renderProductDetailPage(req, res) {
     // get product id from request params
 
-    const productId = req.params.id;
+    // const productId = req.params.id;
 
-    const product = products.find((product) => product.id === productId);
+    const product = {};
 
     return res.render("product/detail", {
         layout: "./layouts/main",
