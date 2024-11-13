e2697b8ec1a4494d9733c1b68f8a41740daa1851
Linh <caonhatlinh1312@gmail.com>
2024-11-13 20:02:07 +0700

    feat(oder): admin: order list, order detail, update status order

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -5,21 +5,20 @@ import env from "dotenv";
 
 env.config();
 
-// const VIEW_OPTIONS = {
-//     ADMIN_CREATE: {
-//         layout: "./layouts/admin",
-//         title: "Thêm đơn hàng",
-//     },
-//     ADMIN_LIST: {
-//         layout: "./layouts/admin",
-//         title: "Danh sách đơn hàng",
-//     },
-//     ORDER_CANCLE: {
-//         layout: "./layouts/main",
-//         title: "Product detail",
-//     },
-
-// };
+const VIEW_OPTIONS = {
+    ADMIN_CREATE: {
+        layout: "./layouts/admin",
+        title: "Thêm đơn hàng",
+    },
+    ADMIN_LIST: {
+        layout: "./layouts/admin",
+        title: "Danh sách đơn hàng",
+    },
+    ORDER_DETAIL: {
+        layout: "./layouts/admin",
+        title: "chi tiết đơn hàng",
+    },
+};
 export const cancelOrder = async (req, res) => {
     const { id: orderId } = req.params;
     const order = await DonHangModel.findById(orderId);
@@ -50,4 +49,94 @@ export const cancelOrder = async (req, res) => {
         }
     );
     return res.redirect("/user/order");
+};
+
+export const renderAdminOrderPage = async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = 10;
+    const skip = (page - 1) * limit;
+
+    const orders = await DonHangModel.find()
+        .populate({ path: "maKhachHang", select: "tenKhachHang anhDaiDien" })
+        .populate({ path: "trangThaiDonHang.maTrangThai", select: "tenTrangThai" })
+        .skip(skip)
+        .limit(limit)
+        .exec();
+
+    const totalOrders = await DonHangModel.countDocuments();
+    const totalPages = Math.ceil(totalOrders / limit);
+
+    const status = await TrangThaiModel.find();
+    const orderList = [];
+
+    for (const order of orders) {
+        const latestStatus = order.trangThaiDonHang[order.trangThaiDonHang.length - 1];
+        const id = latestStatus.maTrangThai._id;
+        const statusOrder = status.findIndex((status) => status._id.equals(id));
+        orderList.push({
+            ...order._doc,
+            trangThai: statusOrder,
+        });
+    }
+
+    return res.render("admin/order", {
+        ...VIEW_OPTIONS.ADMIN_LIST,
+        orders: orderList,
+        statuses: status,
+        currentPage: page,
+        totalPages: totalPages,
+        filters: req.query,
+    });
+};
+
+export const renderAdminOrderDetailPage = async (req, res) => {
+    const { id: orderId } = req.params;
+    const order = await DonHangModel.findById(orderId)
+        .populate({ path: "maKhachHang", select: "tenKhachHang anhDaiDien email " })
+        .populate({ path: "trangThaiDonHang.maTrangThai", select: "maTrangThai tenTrangThai" })
+        .populate({ path: "chiTietDonHang.maSanPham", select: "tenSanPham giaSanPham hinhAnhDaiDien" })
+        .populate({ path: "chiTietDonHang.maKichCoSanPham", select: "tenKichCo" })
+        .exec();
+    const status = await TrangThaiModel.find();
+    return res.render("admin/order/detail", {
+        ...VIEW_OPTIONS.ORDER_DETAIL,
+        order: order,
+        statuses: status,
+    });
+};
+
+export const updateOrderStatus = async (req, res) => {
+    const { id: orderId } = req.params;
+    console.log(orderId);
+    const { status: statusId } = req.body;
+    const order = await DonHangModel.findById(orderId);
+
+    if (!order) {
+        return res.redirect("/admin/order/");
+    }
+
+    const latestStatus = order.trangThaiDonHang[order.trangThaiDonHang.length - 1];
+    const status = await TrangThaiModel.find();
+    const statusCodes = latestStatus.maTrangThai.toString();
+
+    if (statusCodes === statusId) {
+        return res.redirect("/admin/order/detail/" + orderId);
+    }
+
+    const nextStatus = status.find((s) => s._id.equals(statusId));
+
+    await DonHangModel.findOneAndUpdate(
+        { maDonHang: orderId },
+        {
+            $push: {
+                trangThaiDonHang: {
+                    maTrangThai: nextStatus._id,
+                    _id: nextStatus._id,
+                    thoiGian: new Date(),
+                },
+            },
+        }
+    );
+
+    return res.redirect("/admin/order/detail/" + orderId);
 };
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -9,6 +9,11 @@ import brandRoutes from "./brand.route.js";
 import supplierRoutes from "./supplier.route.js";
 import categoryRoutes from "./category.route.js";
 import statusRoutes from "./status.route.js";
+import {
+    renderAdminOrderPage,
+    renderAdminOrderDetailPage,
+    updateOrderStatus,
+} from "../controllers/order.controller.js";
 import multer from "multer";
 const uploadFile = multer({ storage: multer.memoryStorage() });
 
@@ -34,5 +39,7 @@ router.use("/status", statusRoutes);
 
 router.get("/product", renderAdminProductPage);
 router.get("/product/create", renderAdminCreateProductPage);
-
+router.get("/order", renderAdminOrderPage);
+router.get("/order/detail/:id", renderAdminOrderDetailPage);
+router.post("/order/updateStatus/:id", updateOrderStatus);
 export default router;
